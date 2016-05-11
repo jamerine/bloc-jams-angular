9dea1cc3241193d9dfbdd6c3b959465b8e428996
Jason Amerine <amerine.19@gmail.com>
2016-05-11 14:49:48 -0400

    Checkpoint 6 Services 1: Completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,22 +1,9 @@
 (function() {
-    function AlbumCtrl() {
-        this.albumData = {
-            title: 'The Colors',
-            artist: 'Pablo Picasso',
-            label: 'Cubism',
-            year: '1881',
-            albumArtUrl: 'assets/images/album_covers/01.png',
-            songs: [
-                { title: 'Blue', duration: '4:26', audioUrl: '/assets/music/blue' },
-                { title: 'Green', duration: '3:14', audioUrl: '/assets/music/green' },
-                { title: 'Red', duration: '5:01', audioUrl: '/assets/music/red' },
-                { title: 'Pink', duration: '3:21', audioUrl: '/assets/music/pink' },
-                { title: 'Magenta', duration: '2:15', audioUrl: '/assets/music/magenta' }  
-            ]
- };
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum();
     }
     
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
