1480b7c9e85f980e0f2daa185f8240ef0090b779
Jason Amerine <amerine.19@gmail.com>
2016-05-11 14:11:38 -0400

    Assignment 5 Controllers: Completed

---
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,22 @@
+(function() {
+    function AlbumCtrl() {
+        this.albumData = {
+            title: 'The Colors',
+            artist: 'Pablo Picasso',
+            label: 'Cubism',
+            year: '1881',
+            albumArtUrl: 'assets/images/album_covers/01.png',
+            songs: [
+                { title: 'Blue', duration: '4:26', audioUrl: '/assets/music/blue' },
+                { title: 'Green', duration: '3:14', audioUrl: '/assets/music/green' },
+                { title: 'Red', duration: '5:01', audioUrl: '/assets/music/red' },
+                { title: 'Pink', duration: '3:21', audioUrl: '/assets/music/pink' },
+                { title: 'Magenta', duration: '2:15', audioUrl: '/assets/music/magenta' }  
+            ]
+ };
+    }
+    
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', AlbumCtrl);
+})();
